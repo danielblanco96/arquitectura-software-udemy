8f0bad3114c7bf75639103a9d724c735b12e3c69
Daniel Blanco <dablancal96@gmail.com>
2022-05-07 16:11:58 +0100

    Remove imports

--- cqrs/advanced-cqrs/src/main/java/com/danielblanco/arquitecturasmodernas/cqrs/advanced/query/service/QueryService.java
+++ cqrs/advanced-cqrs/src/main/java/com/danielblanco/arquitecturasmodernas/cqrs/advanced/query/service/QueryService.java
@@ -5,8 +5,6 @@ import com.danielblanco.arquitecturasmodernas.cqrs.advanced.query.repository.Pos
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
